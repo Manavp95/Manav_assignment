5b4f3917e2bceed49e4bcf01f423b40f17577c77
Manavp95 <161435908+Manavp95@users.noreply.github.com>
2024-03-03 16:55:51 +0530

    Add files via upload

---
+++ Python_operators.js
@@ -0,0 +1,53 @@
+a  = 3
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)  
+print(a/b)
+
+# Various assignment operations on a variable
+a = 5
+a += 5
+a -= 5
+a *= 5
+a /= 5
+a %= 5
+a //= 5
+a **= 5
+
+#copare two numbers and print results
+a = 5
+b = 10 
+print (a ==b)
+print (a != b)
+
+
+# checking conditions using logical operators 
+a = 5
+b = 10
+c = 20
+print(a > b and b < c)
+print(a > b or b < c)
+print(not(a > b and b < c))
+
+
+#Check identity of variables
+a = 5
+b = 5
+print(a is b)
+print(a is not b)
+
+
+#  unary operators to change the sign of a number
+a = 5
+print(+a)
+print(-a)
+
+# ternary operator to sssign values based on conditions 
+a = 5
+b = 10
+if a >=b:
+  print("a is greater than b")
+else:
+  print("a is less than b")
+
